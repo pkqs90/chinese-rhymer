36c2b4d6e9edec7b8524b087815701183d4e73db
pkqs90 <pkqs90@gmail.com>
2017-08-16 16:57:18 +0800

    add /chinese-rhymer/ prefix

---
+++ _next/5a3a3c03-8bfd-4e5f-9873-db77e5b2ac7a/page/_error/index.js
@@ -0,0 +1,7 @@
+
+          window.__NEXT_REGISTER_PAGE('/_error', function() {
+            var comp = module.exports =
+webpackJsonp([3],[],[356]);
+            return { page: comp.default }
+          })
+        
