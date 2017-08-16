352b92ec4b2e1f517f75a74a7abcfe0b77adaecb
pkqs90 <pkqs90@gmail.com>
2017-08-16 17:00:04 +0800

    remove .next directory

--- .next/bundles/pages/_document.js
+++
@@ -1,7 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/_document', function() {
-            var comp = module.exports =
-webpackJsonp([4],[],[356]);
-            return { page: comp.default }
-          })
-        
